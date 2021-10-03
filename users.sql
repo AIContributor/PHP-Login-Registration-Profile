ac50946bf4b37cd41e31b341c7123e4ae6dffa6a
IrakLi Nishnianidze <78401272+iraklinish@users.noreply.github.com>
2021-10-03 09:35:05 +0400

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` text CHARACTER SET utf8 NOT NULL,
   `password` text CHARACTER SET utf8 NOT NULL,
   `full_name` text CHARACTER SET utf8 NOT NULL,
